024cec78703acc6f011076f48b72a5ed692c7acf
henry22 <henry811010@gmail.com>
2020-04-26 11:18:31 +0800

    Install body-parser

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const handlebars = require('express-handlebars') // 引入 handlebars
 const db = require('./models') // 引入資料庫
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
 app.engine('handlebars', handlebars({defaultLayout: 'main'})) // Handlebars 註冊樣板引擎
 app.set('view engine', 'handlebars') // 設定使用 Handlebars 做為樣板引擎
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.listen(port, () => {
   db.sequelize.sync()
