98df96c51915f542aa01302522e439f403540746
henry22 <henry811010@gmail.com>
2020-04-26 23:30:03 +0800

    Add isAdmin to users table

--- models/user.js
+++ models/user.js
@@ -3,7 +3,8 @@ module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     name: DataTypes.STRING,
     email: DataTypes.STRING,
-    password: DataTypes.STRING
+    password: DataTypes.STRING,
+    isAdmin: DataTypes.BOOLEAN // 加入 isAdmin 欄位
   }, {});
   User.associate = function(models) {
     // associations can be defined here
