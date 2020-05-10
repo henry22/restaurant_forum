ff522d0bebe28634151abb7579c5250fe47f2d1b
henry22 <henry811010@gmail.com>
2020-05-10 22:50:04 +0800

    Define category model charset

--- models/category.js
+++ models/category.js
@@ -1,6 +1,8 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Category = sequelize.define('Category', {
+    charset: 'utf8',
+    collate: 'utf8_general_ci',
     name: DataTypes.STRING
   }, {});
   Category.associate = function(models) {
