6c5c866561165f1c3fe445732f7786c95207c60f
henry22 <henry811010@gmail.com>
2020-05-10 22:41:01 +0800

    Add fix db charset migration

--- seeders/20200502153020-my-seed-file.js
+++ seeders/20200502153020-my-seed-file.js
@@ -37,7 +37,7 @@ module.exports = {
         .map((item, index) =>
           ({
             id: index + 1,
-            name: '中式料理',
+            name: item,
             createdAt: new Date(),
             updatedAt: new Date()
           })
