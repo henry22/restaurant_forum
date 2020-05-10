d4a1fe9b1095c5f576af518de4acd83c2c1cb8f1
henry22 <henry811010@gmail.com>
2020-05-10 21:56:24 +0800

    Change bulkInsert Categories name for testing

--- seeders/20200502153020-my-seed-file.js
+++ seeders/20200502153020-my-seed-file.js
@@ -37,7 +37,7 @@ module.exports = {
         .map((item, index) =>
           ({
             id: index + 1,
-            name: item,
+            name: 'test',
             createdAt: new Date(),
             updatedAt: new Date()
           })
