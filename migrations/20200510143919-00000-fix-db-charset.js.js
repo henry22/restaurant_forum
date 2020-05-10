993c4adf918fdb4f8227d9e5803e926c51b81409
henry22 <henry811010@gmail.com>
2020-05-11 07:35:37 +0800

    Change fix db charset migration to utf8

--- migrations/20200510143919-00000-fix-db-charset.js.js
+++ migrations/20200510143919-00000-fix-db-charset.js.js
@@ -4,7 +4,7 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.sequelize.query(
       `ALTER DATABASE ${queryInterface.sequelize.config.database}
-      CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;`
+      CHARACTER SET utf8 COLLATE utf8_general_ci;`
     )
   },
 
