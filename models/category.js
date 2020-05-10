f4f699910d8cc66967eeb7477826121ce5029b24
henry22 <henry811010@gmail.com>
2020-05-10 23:32:29 +0800

    Remove attributes in category model and production config

--- models/category.js
+++ models/category.js
@@ -1,8 +1,6 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Category = sequelize.define('Category', {
-    charset: 'utf8',
-    collate: 'utf8_general_ci',
     name: DataTypes.STRING
   }, {});
   Category.associate = function(models) {
