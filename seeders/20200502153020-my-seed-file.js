c9143a46215af0eee8745f4d92c41e8311b231bb
henry22 <henry811010@gmail.com>
2020-05-11 18:20:41 +0800

    Change bootstrap cdn url

--- seeders/20200502153020-my-seed-file.js
+++ seeders/20200502153020-my-seed-file.js
@@ -12,7 +12,8 @@ module.exports = {
         isAdmin: true,
         name: 'root',
         createdAt: new Date(),
-        updatedAt: new Date()
+        updatedAt: new Date(),
+        image: 'https://picsum.photos/250'
       },
       {
         email: 'user1@example.com',
@@ -20,7 +21,8 @@ module.exports = {
         isAdmin: false,
         name: 'user1',
         createdAt: new Date(),
-        updatedAt: new Date()
+        updatedAt: new Date(),
+        image: 'https://picsum.photos/250'
       },
       {
         email: 'user2@example.com',
@@ -28,19 +30,21 @@ module.exports = {
         isAdmin: false,
         name: 'user2',
         createdAt: new Date(),
-        updatedAt: new Date()
+        updatedAt: new Date(),
+        image: 'https://picsum.photos/250'
       }
     ], {})
 
     queryInterface.bulkInsert('Categories',
-      ['中式料理', '日本料理', '義大利料理', '墨西哥料理', '素食料理', '美式料理', '複合式料理'].map((item, index) =>
-        ({
-          id: index + 1,
-          name: item,
-          createdAt: new Date(),
-          updatedAt: new Date()
-        })
-      ), {})
+      ['中式料理', '日本料理', '義大利料理', '墨西哥料理', '素食料理', '美式料理', '複合式料理']
+        .map((item, index) =>
+          ({
+            id: index + 1,
+            name: item,
+            createdAt: new Date(),
+            updatedAt: new Date()
+          })
+        ), {})
 
     return queryInterface.bulkInsert('Restaurants',
       Array.from({ length: 50 }).map(d =>
@@ -53,7 +57,7 @@ module.exports = {
           description: faker.lorem.text(),
           createdAt: new Date(),
           updatedAt: new Date(),
-          CategoryId: Math.floor(Math.random() * 5) + 1
+          CategoryId: Math.floor(Math.random() * 7) + 1
         })
       ), {})
   },
