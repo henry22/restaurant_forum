9df98a8bf0298f13bb9fd4181444cd8d3890552b
henry22 <henry811010@gmail.com>
2020-05-12 17:59:13 +0800

    Add comment feature in user page

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,8 @@
 const bcrypt = require('bcryptjs')
 const db = require('../models')
 const User = db.User
+const Comment = db.Comment
+const Restaurant = db.Restaurant
 
 const imgur = require('imgur-node-api')
 const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
@@ -46,11 +48,17 @@ const userController = {
     res.redirect('/signin')
   },
   getUser: (req, res) => {
-    return User.findByPk(req.user.id)
+    return User.findByPk(req.user.id, {
+      include: {
+        model: Comment,
+        include: Restaurant
+      }
+    })
       .then(user => {
         return res.render('user', {
           user: user.toJSON(),
-          editProfile: false
+          editProfile: false,
+          hasCommentRestaurants: user.dataValues.Comments.length > 0
         })
       })
       .catch(err => {
