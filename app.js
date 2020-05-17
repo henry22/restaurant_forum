37d2a80742972208a017a1a1e31b35c8571dcce6
henry22 <henry811010@gmail.com>
2020-05-17 08:16:31 +0800

    Refactor router

--- app.js
+++ app.js
@@ -45,4 +45,4 @@ app.listen(port, () => {
 })
 
 // 引入 routes 並將 app 傳進去，讓 routes 可以用 app 這個物件來指定路由
-require('./routes')(app, passport) // 把 passport 傳入 routes
+require('./routes')(app) // 把 passport 傳入 routes
