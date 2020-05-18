f3c82f153450c0b4edf3dbf45e626cedeeda6f13
henry22 <henry811010@gmail.com>
2020-05-18 21:27:54 +0800

    Install jsonwebtoken & passport-jwt

---
+++ controllers/api/userController.js
@@ -0,0 +1,15 @@
+const bcrypt = require('bcryptjs')
+const db = require('../../models')
+const User = db.User
+
+// JWT
+const jwt = require('jsonwebtoken')
+const passportJWT = require('passport-jwt')
+const ExtractJwt = passportJWT.ExtractJwt
+const JwtStrategy = passportJWT.Strategy
+
+const userController = {
+
+}
+
+module.exports = userController
