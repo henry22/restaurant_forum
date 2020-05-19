b869faebd59fbe6c86c4516de59c535c9066443e
henry22 <henry811010@gmail.com>
2020-05-19 11:04:14 +0800

    Add authenticated and authenticatedAdmin

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const db = require('./models') // 引入資料庫
 const bodyParser = require('body-parser')
 const session = require('express-session')
 const flash = require('connect-flash')
-const passport = require('./config/passport')
 const methodOverride = require('method-override')
 
 const app = express()
@@ -13,6 +12,7 @@ const port = process.env.PORT || 3000
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
+const passport = require('./config/passport')
 
 app.engine('handlebars', handlebars({
   defaultLayout: 'main',
